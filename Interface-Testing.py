24daae06f2dcbd7c0ae2a46931519ca3548a4434
Lutos-s <3026639434@qq.com>
2026-04-01 22:37:28 +0800

    DAY4-learning

---
+++ Interface-Testing.py
@@ -0,0 +1,13 @@
+# import requests
+
+# url = "https://reqres.in/api/login"
+
+# data = {
+#     "email": "eve.holt@reqres.in",
+#     "password": "cityslicka"
+# }
+
+# res = requests.post(url, json=data)
+
+# print(res.status_code)
+# print(res.text)
--- requests.py
+++
@@ -1,6 +0,0 @@
-import requests
-url= "https://jsonplaceholder.typicode.com/posts"
-response = requests.get(url)
-data = response.json()
-print("天气信息：")
-print(data)
